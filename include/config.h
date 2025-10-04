404a702f0dd26b5d010cb50dfc7b676e548edacd
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-04 23:14:25 +0700

    Add files via upload

---
+++ include/config.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+// SoftAP defaults (SSID will append last 4 MAC chars)
+#define AP_SSID_PREFIX   "WSeries-"
+#define AP_PASS          "wseries123"
+#define AP_CHANNEL       6
+
+// Firmware ID
+#define FW_ID "W0W1"
+
